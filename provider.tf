e732637f445668604689cb13d0c6d625509fdc27
Aleksandar Surbanoski <44127686+networklord@users.noreply.github.com>
2024-12-22 14:24:10 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,21 +1,19 @@
 terraform {
- required_providers {
-   aws = {
-     
-     source  = "hashicorp/aws"
-     version = "~> 5.82.2"
-   }
- }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.82.2"
+    }
+  }
+
+  backend "s3" {
+    bucket = "wctestsurba"
+    key    = "state"
+    region = "us-east-1"
+  }
+}
 
 provider "aws" {
   region     = "us-east-1"
-
+ 
 }
-
- backend "s3" {
-   bucket = "wctestsurba"
-   key    = "state"
-   region = "us-east-1"
- }
-}
-
